594ee8b82c67c7db8f958add3e7661749240b826
mirkobrombin <send@mirko.pm>
2023-03-21 14:39:38 +0100

    use FabrHelperToast for toasts in FabrForm
    also support [fabr-form-response] for static responses

--- src/components/form.js
+++ src/components/form.js
@@ -6,6 +6,8 @@ class FabrForm extends FabrCoreComponent {
     this.eventMap = {
       submit: "onSubmit",
     };
+    this.toastHelper = new FabrHelperToast();
+    this.toastHelper.init(this);
   }
 
   onSubmit(event) {
@@ -13,19 +15,48 @@ class FabrForm extends FabrCoreComponent {
     const formData = new FormData(event.target);
     const formAction = event.target.action;
 
+    if (event.target.hasAttribute("fabr-form-response")) {
+      const responseUrl = event.target.getAttribute("fabr-form-response");
+      this.getResponse(responseUrl)
+        .then((data) => {
+          if (data.status === 200) {
+            if (data.redirect) {
+              window.location.href = data.redirect;
+            } else {
+              this.toastHelper.showToast(data.message, "success", 3000, true);
+            }
+          } else {
+            this.toastHelper.showToast(data.message, "error", 0, true);
+          }
+        })
+        .catch((error) => console.error("Error:", error));
+      return;
+    }
+
     this.submitForm(formAction, formData)
       .then((data) => {
         if (data.status === 200) {
           if (data.redirect) {
             window.location.href = data.redirect;
           } else {
-            showToast(data.message, "success", 3000, true);
+            this.toastHelper.showToast(data.message, "success", 3000, true);
           }
         } else {
-          showToast(data.message, "error", 0, true);
+          this.toastHelper.showToast(data.message, "error", 0, true);
         }
       })
       .catch((error) => console.error("Error:", error));
+  }
+
+  getResponse(responseUrl) {
+    return fetch(responseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 
   submitForm(formAction, formData) {

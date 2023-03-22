dc79ed3a4e115b7b6444dfe4a811bb550a980716
mirkobrombin <send@mirko.pm>
2023-03-22 13:09:46 +0100

    add FabrHelperWatcher
    can be used to whatch for variables changes and perform a callback

---
+++ src/helpers/watcher.js
@@ -0,0 +1,63 @@
+/**
+ * @classdesc Represents a FabrHelperWatcher helper.
+ * @extends FabrHelper
+ */
+class FabrHelperWatcher extends FabrHelper {
+  constructor() {
+    super();
+    this.helperName = "Watcher";
+    this.watchers = {};
+  }
+
+  /**
+   * Starts watching a variable for changes.
+   * @param {any} variable - The variable to watch.
+   * @param {function} func - The function to call when the variable changes.
+   */
+  watch(variable, func) {
+    // @@@IF NOT BUILD@@@
+    this.debugger.log(`Watching ${variable}`);
+    // @@@ENDIF@@@
+
+    this.watchers[variable] = setInterval(() => {
+      if (this.watchers[variable] !== variable) {
+        this.watchers[variable] = variable;
+        func(variable);
+      }
+    }, 100);
+  }
+
+  /**
+   * Stops watching a variable for changes.
+   * @param {any} variable - The variable to stop watching.
+   * @returns {boolean} True if the variable was being watched, false otherwise.
+   */
+  unwatch(variable) {
+    // @@@IF NOT BUILD@@@
+    this.debugger.log(`Unwatching ${variable}`);
+    // @@@ENDIF@@@
+
+    if (this.watchers[variable]) {
+      clearInterval(this.watchers[variable]);
+      delete this.watchers[variable];
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Stops watching all variables for changes.
+   * @returns {boolean} True if any variables were being watched, false otherwise.
+   */
+  unwatchAll() {
+    // @@@IF NOT BUILD@@@
+    this.debugger.log(`Unwatching all`);
+    // @@@ENDIF@@@
+
+    let result = false;
+    for (let variable of Object.keys(this.watchers)) {
+      result = this.unwatch(variable) || result;
+    }
+    return result;
+  }
+}
